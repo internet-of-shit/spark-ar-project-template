ce167761292f0b2bb3eabff0b34fe4fa53263e34
geckse <geckse@users.noreply.github.com>
2020-01-18 16:44:26 +0100

    all the shit 💩 - get it

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+// spark-ar modules
+const Materials = require('Materials')
+const Textures = require('Textures')
+const Patches = require('Patches')
+const Time = require('Time')
+const Shaders = require('Shaders')
+const CameraInfo = require('CameraInfo')
+const R = require('Reactive')
+const console = require('Diagnostics')
